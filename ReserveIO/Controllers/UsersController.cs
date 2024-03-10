5d20667e67486e57f8fda1fa2980f82ca647b993
Evgeny Plotnikov <jakejo457@gmail.com>
2024-03-10 22:10:36 +0500

    Дописан User controller

--- ReserveIO/Controllers/UsersController.cs
+++ ReserveIO/Controllers/UsersController.cs
@@ -1,5 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using ReserveIO.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace ReserveIO.Controllers
 {
@@ -7,14 +11,70 @@ namespace ReserveIO.Controllers
 	[Route("api/[controller]")]
 	public class UsersController : ControllerBase
 	{
+		UsersContext db;
 		public UsersController(UsersContext context) 
 		{
 			db = context;
-			if (!DBNull.Users.Any())
+			if (!db.Users.Any())
 			{
-
+				db.Users.Add(new User { Name = "Tom", Age = 26 });
+				db.Users.Add(new User { Name = "Alice", Age = 31 });
+				db.SaveChanges();
 			}
 
+		}
+		[HttpGet]
+		public async Task<ActionResult<IEnumerable<User>>> Get()
+		{
+			return await db.Users.ToListAsync();
+		}
+		[HttpGet("{id}")]
+		public async Task<ActionResult<User>> Get(int id)
+		{
+			User user = await db.Users.FirstOrDefaultAsync(x => x.Id == id);
+			if (user == null)
+				return NotFound();
+			return new ObjectResult(user);
+		}
+		[HttpPost]
+		public async Task<ActionResult<User>> Post(User user)
+		{
+			if (user == null)
+			{
+				return BadRequest();
+			}
+
+			db.Users.Add(user);
+			await db.SaveChangesAsync();
+			return Ok(user);
+		}
+		[HttpPut]
+		public async Task<ActionResult<User>> Put(User user)
+		{
+			if (user == null)
+			{
+				return BadRequest();
+			}
+			if (!db.Users.Any(x => x.Id == user.Id))
+			{
+				return NotFound();
+			}
+
+			db.Update(user);
+			await db.SaveChangesAsync();
+			return Ok(user);
+		}
+		[HttpDelete("{id}")]
+		public async Task<ActionResult<User>> Delete(int id)
+		{
+			User user = db.Users.FirstOrDefault(x => x.Id == id);
+			if (user == null)
+			{
+				return NotFound();
+			}
+			db.Users.Remove(user);
+			await db.SaveChangesAsync();
+			return Ok(user);
 		}
 	}
 }
