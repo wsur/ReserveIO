0c031e4d7c594d523ef60e31bbf4d82f8f7c6912
Evgeny Plotnikov <jakejo457@gmail.com>
2024-03-10 21:46:24 +0500

    Добавьте файлы проекта.

---
+++ ReserveIO/Controllers/UsersController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ReserveIO.Models;
+
+namespace ReserveIO.Controllers
+{
+	[ApiController]
+	[Route("api/[controller]")]
+	public class UsersController : ControllerBase
+	{
+		public UsersController(UsersContext context) 
+		{
+			db = context;
+			if (!DBNull.Users.Any())
+			{
+
+			}
+
+		}
+	}
+}
---
+++ ReserveIO/Models/UsersContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ReserveIO.Models
+{
+	//Промежуточный класс для сопоставления модели с базой данных
+	public class UsersContext : DbContext
+	{
+		public DbSet<User> Users { get; set; }
+		public UsersContext(DbContextOptions<UsersContext> options)
+			: base(options)
+		{
+			Database.EnsureCreated();
+		}
+	}
+}
