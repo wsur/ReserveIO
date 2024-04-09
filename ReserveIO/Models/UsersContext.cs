63e292da15c688d51d0b5a46ff00a1d017fce08c
Evgeny Plotnikov <jakejo457@gmail.com>
2024-04-09 21:53:58 +0500

    Добавлены начальные данные для дб и исправлена строка подключения. Теперь нет проблем с бд. В файле UserContext можно раскоментировать строку EnsureDeleted, если возникли проблемы с бд и запустить -- приложение создать полностью новую бд.

--- ReserveIO/Models/UsersContext.cs
+++ ReserveIO/Models/UsersContext.cs
@@ -9,13 +9,26 @@ namespace ReserveIO.Models
 		public UsersContext(DbContextOptions<UsersContext> options)
 			: base(options)
 		{
+			//Database.EnsureDeleted();//Странная вещь. Зачем это нужно, чтобы каждый раз новую создавать? В чём смысл?
 			Database.EnsureCreated();
-			if (!Users.Any())
+/*			if (!Users.Any())
 			{
 				Users.Add(new User { Name = "Tom", Age = 26 });
 				Users.Add(new User { Name = "Alice", Age = 31 });
 				SaveChanges();
-			}
+			}*/
+
+		}
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.Entity<User>().HasData(
+				new User[]
+				{
+					new User { Id=1, Name="Tom", Age = 23},
+					new User { Id = 2, Name = "Alice", Age = 26 },
+					new User { Id = 3, Name = "Sam", Age = 28 }
+				});
+			base.OnModelCreating(modelBuilder);
 		}
 	}
 }
