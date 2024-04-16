842506f32ee68b8b160b031b67489e1e0e287d21
Evgeny Plotnikov <jakejo457@gmail.com>
2024-04-16 23:14:31 +0500

    Добавлены токены отмены во все HTTP-запросы

--- ReserveIO/Controllers/UsersController.cs
+++ ReserveIO/Controllers/UsersController.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using ReserveIO.Models;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ReserveIO.Controllers
@@ -29,15 +30,15 @@ namespace ReserveIO.Controllers
 			return await db.Users.ToListAsync(cancellationToken);//добавлен токен, который позволяет отменить запрос
 		}
 		[HttpGet("{id}")]
-		public async Task<ActionResult<User>> Get(int id)
+		public async Task<ActionResult<User>> Get(int id, CancellationToken cancellationToken)
 		{
-			User user = await db.Users.FirstOrDefaultAsync(x => x.Id == id);
+			User user = await db.Users.FirstOrDefaultAsync(x => x.Id == id, cancellationToken);
 			if (user == null)
 				return NotFound();
 			return new ObjectResult(user);
 		}
 		[HttpPost]
-		public async Task<ActionResult<User>> Post(User user)
+		public async Task<ActionResult<User>> Post(User user, CancellationToken cancellationToken)
 		{
 			if (user == null)
 			{
@@ -45,11 +46,11 @@ namespace ReserveIO.Controllers
 			}
 
 			db.Users.Add(user);
-			await db.SaveChangesAsync();
+			await db.SaveChangesAsync(cancellationToken);
 			return Ok(user);
 		}
 		[HttpPut]
-		public async Task<ActionResult<User>> Put(User user)
+		public async Task<ActionResult<User>> Put(User user, CancellationToken cancellationToken)
 		{
 			if (user == null)
 			{
@@ -61,15 +62,15 @@ namespace ReserveIO.Controllers
 			}
 
 			db.Update(user);
-			await db.SaveChangesAsync();
+			await db.SaveChangesAsync(cancellationToken);
 			return Ok(user);
 		}
 		[HttpDelete("{id}")]
-		public async Task<ActionResult<User>> Delete(int id)
+		public async Task<ActionResult<User>> Delete(int id, CancellationToken cancellationToken)
 		{
 			User user = new User { Id = id};//создание объекта-заглушки
 			var result = db.Remove(user);
-			await db.SaveChangesAsync();
+			await db.SaveChangesAsync(cancellationToken);
 			if (result != null)
 			{
 				return Ok();
