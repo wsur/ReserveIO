8636e4dfa80ecc0516c6b1dd863cca32b9a8907d
Evgeny Plotnikov <jakejo457@gmail.com>
2024-04-17 23:31:36 +0500

    Добавлена выходная документация XML для контроллера пользователей

--- ReserveIO/Controllers/UsersController.cs
+++ ReserveIO/Controllers/UsersController.cs
@@ -24,6 +24,11 @@ namespace ReserveIO.Controllers
 						}*/
 
 		}
+		/// <summary>
+		/// Methon get is used for getting all elements from database
+		/// </summary>
+		/// <param name="cancellationToken">There is cancellation token</param>
+		/// <returns>All <see cref="T:ReserveIO.Models.User"/> from the database</returns>
 		[HttpGet]
 		public async Task<ActionResult<IEnumerable<User>>> Get(CancellationToken cancellationToken)
 		{
@@ -37,6 +42,12 @@ namespace ReserveIO.Controllers
 				return NotFound();
 			return new ObjectResult(user);
 		}
+		/// <summary>
+		/// Method POST is used for add brand-new user to database without writing an user id
+		/// </summary>
+		/// <param name="user">Input user</param>
+		/// <param name="cancellationToken">There is cancellation token</param>
+		/// <returns><see cref="T:ReserveIO.Models.User"/></returns>
 		[HttpPost]
 		public async Task<ActionResult<User>> Post(User user, CancellationToken cancellationToken)
 		{
@@ -49,6 +60,12 @@ namespace ReserveIO.Controllers
 			await db.SaveChangesAsync(cancellationToken);
 			return Ok(user);
 		}
+		/// <summary>
+		/// Method PUT is used for modify existing users in the database
+		/// </summary>
+		/// <param name="user">Input user</param>
+		/// <param name="cancellationToken">There is cancellation token</param>
+		/// <returns><see cref="T:ReserveIO.Models.User"/> with given id from the database if succeded</returns>
 		[HttpPut]
 		public async Task<ActionResult<User>> Put(User user, CancellationToken cancellationToken)
 		{
@@ -65,6 +82,12 @@ namespace ReserveIO.Controllers
 			await db.SaveChangesAsync(cancellationToken);
 			return Ok(user);
 		}
+		/// <summary>
+		/// Method Delete is used for Deleting user that exist in database
+		/// </summary>
+		/// <param name="id">Id for user that we want to delete from the database</param>
+		/// <param name="cancellationToken">There is cancellation token</param>
+		/// <returns><see cref="M:ControllerBase.OK()"/> if operation is succeded</returns>
 		[HttpDelete("{id}")]
 		public async Task<ActionResult<User>> Delete(int id, CancellationToken cancellationToken)
 		{
