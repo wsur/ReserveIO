15d805133440c6680d26617b52baa3b0995b0c10
Плотников Евгений Сергеевич <plotnikov.es@prg-it.ru>
2024-05-21 15:20:54 +0500

    Обновлены методы PUT у UserRoles и ServiceInfo сущностей

---
+++ ReserveIO/Migrations/20240521101856_UpdateUserRole.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ReserveIO.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateUserRole : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "UserRoleId",
+                table: "UserRoles",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserRoleId",
+                table: "UserRoles");
+        }
+    }
+}
